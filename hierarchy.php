297c5b4eeead66046a457a7b4def10f7d666005e
Patrick B <pjbrof@gmail.com>
2015-09-13 13:46:34 -0400

    initial commit

---
+++ hierarchy.php
@@ -0,0 +1,6 @@
+<?php 
+
+	/* This file will scan the file system and retreive the hierarchy of the file subset and associated files. 
+	It will also get details of if the it is a file or directory as well as the file or directory size for later use. */
+
+?>
